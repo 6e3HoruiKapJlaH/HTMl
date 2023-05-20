769fa764a6cc5f3b878b17dbfff26a865b95801f
6e3HoruiKapJlaH <vedmacrl@gmail.com>
2023-05-20 14:26:16 +0300

    changes

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('header1').addEventListener('click', function() {
+    document.getElementById('textFrame').innerText = 'Header 1 clicked';
+});
+document.getElementById('header2').addEventListener('click', function() {
+    document.getElementById('textFrame').innerText = 'Header 2 clicked';
+});
+document.getElementById('header3').addEventListener('click', function() {
+    document.getElementById('textFrame').innerText = 'Header 3 clicked';
+});
