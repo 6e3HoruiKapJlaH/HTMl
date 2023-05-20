ba643f7a592cac00e8745b32750e75c4bb9731bb
Yevhenii Kashtalian <djin.kashtalyan@gmail.com>
2023-05-20 15:29:06 +0300

    fix header1 text

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById('header1').addEventListener('click', function() {
-    document.getElementById('textFrame').innerText = 'Тема: СТРУКТУРА HTML-ДОКУМЕНТА. ВИБІР ПРЕДМЕТНОЇ ГАЛУЗІ. РОБОТА З ПОСИЛАННЯМИ, ТАБЛИЦЯМИ, ЗОБРАЖЕННЯМИ, СПИСКАМИ в HTML-ДОКУМЕНТІ.';
+    document.getElementById('textFrame').innerText = 'Тема: СТРУКТУРА HTML-ДОКУМЕНТА. ВИБІР ПРЕДМЕТНОЇ ГАЛУЗІ. РОБОТА З ПОСИЛАННЯМИ, ТАБЛИЦЯМИ, ЗОБРАЖЕННЯМИ, СПИСКАМИ в HTML-ДОКУМЕНТІ.\n' +
+    '\n Мета: придбати практичні навички роботи з HTML-документом, таблицями,\n' +
+    'зображеннями, посиланнями, списками, формами.\n' +
+    'Створити шаблон звітного HTML-документом для відображення\n' +
+    'результатів роботи всіх лабораторних робіт.';
 });
 document.getElementById('header2').addEventListener('click', function() {
     document.getElementById('textFrame').innerText = 'Header 2 clicked';
