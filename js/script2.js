ea33485465f72c87b34e0261e2d505c42de307ab
6e3HoruiKapJlaH <vedmacrl@gmail.com>
2023-05-21 15:32:51 +0300

    added onclick

--- js/script2.js
+++ js/script2.js
@@ -8,3 +8,16 @@ selectorLi.addEventListener('click', function() {
     submenu.style.display = 'none';
   }
 });
+
+document.getElementById('header1').addEventListener('click', function() {
+  var htmlCode = `<!DOCTYPE html>    `
+  htmlCode.color = '#2C81B7';
+  var cssCode = `p{
+    background-color: #58c93a;
+      text-align: end;
+  }`;
+     cssCode.color = 'blue';
+      
+      document.getElementById('textFrame').innerText = htmlCode;
+      document.getElementById('textFrame').appendChild(cssCode);
+  });
