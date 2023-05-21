9a3cc884530c5def1d39676bf35d18635fd9fc51
Yevhenii Kashtalian <djin.kashtalyan@gmail.com>
2023-05-21 15:57:47 +0300

    added code header1

--- js/script2.js
+++ js/script2.js
@@ -10,14 +10,36 @@ selectorLi.addEventListener('click', function() {
 });
 
 document.getElementById('header1').addEventListener('click', function() {
-  var htmlCode = `<!DOCTYPE html>    `
-  htmlCode.color = '#2C81B7';
-  var cssCode = `p{
+  var htmlCode = `
+  <div class="post">
+    <h1 class="title">
+      <a href="#">Duis aute irure dolor in henderit in voluptate.</a>
+    </h1>
+
+    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+
+    <a class="read_more" href="#">Continue Reading <i class="read_more_arrow"></i> </a>
+  </div>`;
+
+  var cssCode = `p {
     background-color: #58c93a;
-      text-align: end;
+    text-align: end;
+    color: blue;
   }`;
-     cssCode.color = 'blue';
-      
-      document.getElementById('textFrame').innerText = htmlCode;
-      document.getElementById('textFrame').appendChild(cssCode);
-  });
+
+  var textFrame = document.getElementById('textFrame');
+  textFrame.innerHTML = ''; 
+
+  var htmlElement = document.createElement('pre');
+  htmlElement.style.color = '#2C81B7';
+  htmlElement.textContent = htmlCode;
+  textFrame.appendChild(htmlElement);
+
+  var cssElement = document.createElement('pre');
+  cssElement.style.color = 'blue';
+  cssElement.textContent = cssCode;
+  textFrame.appendChild(cssElement);
+});
+
